fb3d0aa85d01b85eb826a18f336fffe7caf0b3a7
Utsav Ojha <utsavojha@Utsavs-MacBook-Air.local>
2025-11-29 10:23:01 -0500

    Show image in root endpoint

--- flask/main.py
+++ flask/main.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def show_image():
+    return '''
+    <h1>My Image</h1>
+    <img src="/static/image.jpg" width="400">
+    '''
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8888)
